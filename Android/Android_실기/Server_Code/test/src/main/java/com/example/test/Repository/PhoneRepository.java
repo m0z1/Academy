28a8200dffd84a7b3f9a151ac68a6a92809f689c
m0z1 <9818mo@gmail.com>
2023-10-19 10:07:58 +0900

    2023-10-19

--- Android/Android_실기/Server_Code/test/src/main/java/com/example/test/Repository/PhoneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.test.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.test.Model.Phone;
-
-public interface PhoneRepository extends JpaRepository<Phone, Long> {
-
-}
